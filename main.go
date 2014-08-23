0556d0c222be4f0ebf84c4865a45e0b72ac143fb
Teoman Soygul <teo@soygul.com>
2014-08-23 23:57:04 +0200

    add gcm css init.

--- main.go
+++ main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
+
+const (
+	GCM_CLIENT_ID = ""
+	GCM_API_KEY   = ""
+
+	TEST_REG_ID = ""
+)
 
 func main() {
-	fmt.Println("NBusy messege server ready.")
+	ccsClient, err := ccs.New(GCM_CLIENT_ID, GCM_API_KEY, false)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	msgCh := make(chan map[string]interface{})
+	errCh := make(chan error)
+
+	go ccsClient.Recv(msgCh, errCh)
+
+	ccsMessage := ccs.NewMessage(TEST_REG_ID)
+	ccsMessage.SetData("hello", "world")
+	ccsMessage.CollapseKey = ""
+	ccsMessage.TimeToLive = 0
+	ccsMessage.DelayWhileIdle = true
+	ccsClient.Send(ccsMessage)
+
+	for {
+		select {
+		case err := <-errCh:
+			fmt.Println("err:", err)
+		case msg := <-msgCh:
+			fmt.Println("msg:", msg)
+		}
+	}
+
+	fmt.Print("NBusy messege server started.")
 }
