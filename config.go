0a4b30fbb02e3fae2b4be4207a268bba4455961f
Teoman Soygul <teo@soygul.com>
2014-08-26 17:02:19 +0200

    add config module

---
+++ config.go
@@ -0,0 +1,12 @@
+package main
+
+// GCM type describes the Google Cloud Messaging parameters as described here: https://developer.android.com/google/gcm/gs.html
+type GCM struct {
+	CCSEndpoint string
+	SenderID    string
+	APIKey      string
+}
+
+func config() {
+
+}
--- main.go
+++ main.go
@@ -4,19 +4,31 @@ import (
 	"fmt"
 	"log"
 	"os"
+
 	"github.com/soygul/nbusy-server/gcm/ccs"
 )
 
 const (
-	GCM_CCS_ENDPOINT = "gcm.googleapis.com:5235"
+	GO_ENV = "development"
+	GCM_CCS_ENDPOINT         = "gcm.googleapis.com:5235"
 	GCM_CCS_STAGING_ENDPOINT = "gcm-staging.googleapis.com:5236"
-	GCM_SENDER_ID = ""
-	GOOGLE_API_KEY = ""
-	GCM_TEST_REG_ID = ""
+	GCM_SENDER_ID            = ""
+	GOOGLE_API_KEY           = ""
+	GCM_TEST_REG_ID          = ""
 )
 
+// GCM type describes the Google Cloud Messaging parameters as described here: https://developer.android.com/google/gcm/gs.html
+type GCM struct {
+	CCSEndpoint string
+	SenderID    string
+	APIKey      string
+}
+
 func main() {
-	os.Getenv("GCM_CLIENT_ID")
+	gcm := GCM{CCSEndpoint: os.Getenv("GCM_CCS_ENDPOINT"), SenderID: os.Getenv("GCM_SENDER_ID"), APIKey: os.Getenv("GOOGLE_API_KEY")}
+
+
+
 	ccsClient, err := ccs.New(GCM_SENDER_ID, GOOGLE_API_KEY, false)
 	if err != nil {
 		log.Fatal(err)
@@ -27,12 +39,14 @@ func main() {
 
 	go ccsClient.Recv(msgCh, errCh)
 
-	ccsMessage := ccs.NewMessage(GCM_TEST_REG_ID)
+	ccsMessage := ccs.NewMessage("GCM_TEST_REG_ID")
 	ccsMessage.SetData("hello", "world")
 	ccsMessage.CollapseKey = ""
 	ccsMessage.TimeToLive = 0
 	ccsMessage.DelayWhileIdle = true
 	ccsClient.Send(ccsMessage)
+
+	fmt.Print("NBusy messege server started.")
 
 	for {
 		select {
@@ -42,6 +56,4 @@ func main() {
 			fmt.Println("msg:", msg)
 		}
 	}
-
-	fmt.Print("NBusy messege server started.")
 }
