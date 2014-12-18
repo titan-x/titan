7263c5b80163e6bca9853bbaa6a7716fbb7be428
Teoman Soygul <teo@soygul.com>
2014-12-18 01:35:11 +0100

    add basic chats map

--- main.go
+++ main.go
@@ -6,12 +6,17 @@ import (
 	"github.com/nbusy/gcm/ccs"
 )
 
+// Chat is a private or group chat
+type Chat struct {
+}
+
+var chats = make(map[string]Chat)
+
 func main() {
 	c, err := ccs.Connect(Conf.GCM.CCSHost, Conf.GCM.SenderID, Conf.GCM.getAPIKey(), Conf.App.Debug)
 	if err != nil {
 		log.Fatalln("Failed to connect to GCM CCS with error:", err)
 	}
-
 	log.Println("NBusy message server started.")
 
 	for {
@@ -19,6 +24,7 @@ func main() {
 		if err != nil {
 			log.Println("CCS sent error:", err)
 		}
+
 		go readHandler(m)
 	}
 }
