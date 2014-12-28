f84622b6daf4783631a0042e69a66e0f7cbf97a2
Teoman Soygul <teo@soygul.com>
2014-12-28 13:39:07 +0100

    device per user details

--- main.go
+++ main.go
@@ -6,11 +6,17 @@ import (
 	"github.com/nbusy/gcm/ccs"
 )
 
-// Chat is a private or group chat
+// Chat is a private or group chat.
 type Chat struct {
+	ID    uint64
+	Users []uint64
 }
 
 var chats = make(map[string]Chat)
+
+// user -> id (user or chat id) -> message
+// delivery status -> user
+// read status -> user
 
 func main() {
 	c, err := ccs.Connect(Conf.GCM.CCSHost, Conf.GCM.SenderID, Conf.GCM.getAPIKey(), Conf.App.Debug)
