32b949cda5511e3635aad4dcbb542d2a259de42d
Teoman Soygul <teo@soygul.com>
2014-12-30 04:02:29 +0100

    add note

--- config_test.go
+++ config_test.go
@@ -3,7 +3,9 @@ package main
 import "testing"
 
 func TestConfig(t *testing.T) {
-	if Conf.App.Env != "test" {
+	if Conf.App.Env != "test" || !Conf.App.Debug {
 		t.Error("Config file is not initialized properly for development environment.")
 	}
+
+	// try the init block and see if it covers other files too regardless of execution order
 }
