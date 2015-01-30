5e1bdbebe95ff90d97a96fdf9dd9f2155905be3e
Teoman Soygul <teo@soygul.com>
2015-01-30 14:51:30 +0100

    add cert note

--- main_test.go
+++ main_test.go
@@ -4,3 +4,8 @@ import "testing"
 
 func TestMain(t *testing.T) {
 }
+
+// Generates a TLS certificate for server.
+// Based on sample from http://golang.org/src/crypto/tls/generate_cert.go (Jan 30, 2015).
+func genCert() {
+}
