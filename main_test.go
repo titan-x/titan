a590717fa26a83cae1fc0e52288a40f6b84c2935
Teoman Soygul <teo@soygul.com>
2015-02-06 03:49:28 +0200

    add final certgen code

--- main_test.go
+++ main_test.go
@@ -1,11 +1,59 @@
 package main
 
-import "testing"
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"log"
+	"math/big"
+	"net"
+	"testing"
+	"time"
+)
 
 func TestMain(t *testing.T) {
 }
 
-// Generates a TLS certificate for server.
+// Generate a self-signed X.509 certificate for a TLS server.
 // Based on sample from http://golang.org/src/crypto/tls/generate_cert.go (Jan 30, 2015).
-func genCert() {
+func genCert() x509.Certificate {
+	hosts := []string{"nbusy.com"}
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	notBefore := time.Now()
+	notAfter := notBefore.Add(290 * 365 * 24 * time.Hour) //290 years
+	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
+	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+	if err != nil {
+		log.Fatalf("failed to generate serial number: %s", err)
+	}
+
+	cert := x509.Certificate{
+		IsCA:         true,
+		SerialNumber: serialNumber,
+		Subject: pkix.Name{
+			Organization: []string{"NBusy"},
+		},
+		NotBefore: notBefore,
+		NotAfter:  notAfter,
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+	}
+
+	for _, h := range hosts {
+		if ip := net.ParseIP(h); ip != nil {
+			cert.IPAddresses = append(cert.IPAddresses, ip)
+		} else {
+			cert.DNSNames = append(cert.DNSNames, h)
+		}
+	}
+
+	derBytes, err := x509.CreateCertificate(rand.Reader, &cert, &cert, privKey.PublicKey, privKey)
+	if err != nil {
+		log.Fatalf("Failed to create certificate: %s", err)
+	}
+
+	return cert
 }
