b61e689c0966659177e16eec8da7f8398e5aaa74
Teoman Soygul <teo@soygul.com>
2015-03-11 16:13:35 +0100

    added main example template

---
+++ example_test.go
@@ -0,0 +1,11 @@
+package main_test
+
+// todo: add server.go setup example
+
+// Example demonstrating the use of Devastator server.
+func Example() {
+	go handleMessage()
+}
+
+func handleMessage() {
+}
