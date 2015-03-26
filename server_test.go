f9c8bfdc82e8d35239a131b13deee88cb932c84a
Teoman Soygul <teo@soygul.com>
2015-03-27 00:01:02 +0100

    add test stubs

--- server_test.go
+++ server_test.go
@@ -4,3 +4,8 @@ import "testing"
 
 func TestAuth(t *testing.T) {
 }
+
+func TestStop(t *testing.T) {
+	// t.Fatal("Failed to stop the server gracefully: not all the goroutines were terminated.")
+	// t.Fatal("Failed to stop the server gracefully: server did not wait for ongoing read/write operations.")
+}
