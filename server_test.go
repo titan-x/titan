f028b712d903d2e0aa3c2cd6f8170286c8f56b2c
Teoman Soygul <teo@soygul.com>
2015-03-28 19:49:42 +0100

    add ack test notes

--- server_test.go
+++ server_test.go
@@ -6,16 +6,19 @@ func TestGoogleAuth(t *testing.T) {
 	// t.Fatal("Google+ first sign-in (registration) failed with valid credentials")
 	// t.Fatal("Google+ second sign-in (regular) failed with valid credentials")
 	// t.Fatal("Google+ sign-in passed with invalid credentials")
+	// t.Fatal("Authentication was not ACKed")
 }
 
 func TestClientCertAuth(t *testing.T) {
 	// t.Fatal("Authentication failed with a valid client certificate")
 	// t.Fatal("Authenticated with invalid/expired client certificate")
+	// t.Fatal("Authentication was not ACKed")
 }
 
 func TestTokenAuth(t *testing.T) {
 	// t.Fatal("Authentication failed with a valid token")
 	// t.Fatal("Authenticated with invalid/expired token")
+	// t.Fatal("Authentication was not ACKed")
 }
 
 func TestReceiveOfflineQueue(t *testing.T) {
