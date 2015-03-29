3842aa15c5c5876c3723f3acb874ae7275b270f8
Teoman Soygul <teo@soygul.com>
2015-03-29 14:00:32 +0200

    add read deadline

--- conn.go
+++ conn.go
@@ -1,6 +1,9 @@
 package main
 
-import "crypto/tls"
+import (
+	"crypto/tls"
+	"time"
+)
 
 // Conn is a mobile client connection.
 type Conn struct {
@@ -14,13 +17,18 @@ func (c *Conn) SendMsg(msg *interface{}) error {
 	return nil
 }
 
-// ReadMsg waits for and reads the next message of the TLS connection.
-func (c *Conn) ReadMsg() (msg []byte, err error) {
+// ReadMsg waits for and reads the next message of the TLS connection, up to the given max message size. If max size is not defined,
+// header limit of 4294967295 bytes (4GB) is used.
+func (c *Conn) ReadMsg(maxSize int) (msg []byte, err error) {
 	if c.err != nil {
 		return nil, c.err
 	}
 
-	// first 4 bytes (uint32) is message length header with a maximum of 4294967295 (4GB) with a hard-cap defined by configuration
+	if err = c.conn.SetReadDeadline(time.Now().Add(time.Minute * 5)); err != nil {
+		return
+	}
+
+	// first 4 bytes (uint32) is message length header with a maximum of 4294967295 (4GB) with a hard-cap defined by the parameter
 
 	return
 }
