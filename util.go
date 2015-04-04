4f16ee7af061ba730805bd9fa9ba2defb98af22d
Teoman Soygul <teo@soygul.com>
2015-04-04 16:00:33 +0200

    new cert function

--- util.go
+++ util.go
@@ -10,6 +10,7 @@ import (
 	"math/big"
 	mathrand "math/rand"
 	"net"
+	"strings"
 	"time"
 )
 
@@ -82,10 +83,17 @@ func genCert() (pemBytes, privBytes []byte, err error) {
 // genCert generates a PEM encoded X.509 certificate and private key pair (i.e. 'cert.pem', 'key.pem').
 // This code is based on the sample from http://golang.org/src/crypto/tls/generate_cert.go (taken at Jan 30, 2015).
 // If no private key is provided, the certificate is marked as self-signed CA.
-// host = Comma-separated hostnames and IPs to generate a certificate for
-func genCertNew(host string, validFor time.Duration) (pemBytes, privBytes []byte, err error) {
-	hosts := []string{"localhost"}
-	privKey, err := rsa.GenerateKey(rand.Reader, 512)
+// host = Comma-separated hostnames and IPs to generate a certificate for. i.e. "localhost,127.0.0.1"
+// validFor = Validity period for the certificate. Defaults to time.Duration max (290 years).
+// privKey = Defaults to self-signed CA if not given with a key lenght of 2048.
+// org, cn = Organization
+func genCertNew(host string, validFor time.Duration, privKey *rsa.PrivateKey, org string, cn string) (pemBytes, privBytes []byte, err error) {
+	hosts := strings.Split(host, ",")
+	isCA := false
+	if privKey == nil {
+		privKey, err = rsa.GenerateKey(rand.Reader, 2048)
+		isCA = true
+	}
 	notBefore := time.Now()
 	notAfter := notBefore.Add(290 * 365 * 24 * time.Hour) //290 years
 	serialNumberLimit := new(big.Int).Lsh(big.NewInt(1), 128)
@@ -93,18 +101,25 @@ func genCertNew(host string, validFor time.Duration) (pemBytes, privBytes []byte
 	if err != nil {
 		return nil, nil, fmt.Errorf("failed to generate the certificate serial number: %v", err)
 	}
+	if validFor != 0 {
+		notAfter = notBefore.Add(validFor)
+	}
 
 	cert := x509.Certificate{
-		IsCA:         true,
+		IsCA:         isCA,
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
 			Organization: []string{"localhost"},
 		},
 		NotBefore:             notBefore,
 		NotAfter:              notAfter,
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
 		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
 		BasicConstraintsValid: true,
+	}
+	if isCA {
+		cert.KeyUsage |= x509.KeyUsageCertSign
+		cert.ExtKeyUsage = append(cert.ExtKeyUsage, x509.ExtKeyUsageClientAuth)
 	}
 
 	for _, h := range hosts {
@@ -121,7 +136,9 @@ func genCertNew(host string, validFor time.Duration) (pemBytes, privBytes []byte
 	}
 
 	pemBytes = pem.EncodeToMemory(&pem.Block{Type: "CERTIFICATE", Bytes: derBytes})
-	privBytes = pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privKey)})
+	if isCA {
+		privBytes = pem.EncodeToMemory(&pem.Block{Type: "RSA PRIVATE KEY", Bytes: x509.MarshalPKCS1PrivateKey(privKey)})
+	}
 	return
 }
 
