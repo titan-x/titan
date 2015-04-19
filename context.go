3eb90d341213135e36560fa27423477679bd0924
Teoman Soygul <teo@soygul.com>
2015-04-20 01:25:47 +0200

    fix type override

--- context.go
+++ context.go
@@ -4,12 +4,12 @@ package main
 type Context struct {
 	UserID         uint32 // should be in state
 	IsDisconnected bool
-	error          string
+	err            string
 	Conn           *Conn
 	State          interface{}
 }
 
 // SetError sets a connection error that will be written to the connection before it is closed right before the next read cycle.
 func (c *Context) SetError(err string) {
-	c.error = err
+	c.err = err
 }
