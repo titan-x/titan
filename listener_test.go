8b91d1e7d9daa4a0a909a1b0c094fbaebc43c3dd
Teoman Soygul <teo@soygul.com>
2015-04-21 13:31:08 +0200

    extend listener test

--- listener_test.go
+++ listener_test.go
@@ -5,7 +5,6 @@ import (
 	"crypto/x509"
 	"io"
 	"strconv"
-	"sync"
 	"testing"
 )
 
@@ -15,7 +14,12 @@ func TestLen(t *testing.T) {
 }
 
 func TestListener(t *testing.T) {
-	var wg sync.WaitGroup
+	msg1 := "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	msg2 := "In sit amet lectus felis, at pellentesque turpis."
+	msg3 := "Nunc urna enim, cursus varius aliquet ac, imperdiet eget tellus."
+	msg4 := randString(45000)
+	msg5 := randString(500000)
+
 	host := "localhost:" + Conf.App.Port
 	cert, privKey, _ := genCert("localhost", 0, nil, nil, 512, "localhost", "devastator")
 	listener, err := Listen(cert, privKey, host, Conf.App.Debug)
@@ -25,10 +29,6 @@ func TestListener(t *testing.T) {
 	defer listener.Close()
 
 	go listener.Accept(func(conn *tls.Conn, session *Session, msg []byte) {
-		wg.Add(1)
-		defer wg.Done()
-		// todo: compare sent/incoming messages for equality
-
 		certs := conn.ConnectionState().PeerCertificates
 		if len(certs) > 0 {
 			t.Logf("Client connected with client certificate subject: %v\n", certs[0].Subject)
@@ -50,17 +50,19 @@ func TestListener(t *testing.T) {
 	defer conn.Close()
 
 	send(t, conn, "4\nping")
-	send(t, conn, "56\nLorem ipsum dolor sit amet, consectetur adipiscing elit.")
-	send(t, conn, "49\nIn sit amet lectus felis, at pellentesque turpis.")
-	send(t, conn, "64\nNunc urna enim, cursus varius aliquet ac, imperdiet eget tellus.")
-	send(t, conn, "45000\n"+randString(45000))
+	send(t, conn, "56\n"+msg1)
+	send(t, conn, "56\n"+msg1)
+	send(t, conn, "49\n"+msg2)
+	send(t, conn, "64\n"+msg3)
+	send(t, conn, "45000\n"+msg4)
+	send(t, conn, "56\n"+msg1)
+	send(t, conn, "500000\n"+msg5)
+	send(t, conn, "56\n"+msg1)
 	send(t, conn, "5\nclose")
 
 	// t.Logf("\nconn:\n%+v\n\n", conn)
 	// t.Logf("\nconn.ConnectionState():\n%+v\n\n", conn.ConnectionState())
 	// t.Logf("\ntls.Config:\n%+v\n\n", tlsConf)
-
-	wg.Wait()
 }
 
 func send(t *testing.T, conn *tls.Conn, msg string) {
