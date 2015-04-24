a5e44c9804b5300ca0f9609f6d694393eb6fd70b
Teoman Soygul <teo@soygul.com>
2015-04-24 16:17:51 +0200

    hack: renable listener test with shortcut wait

--- listener_test.go
+++ listener_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"crypto/tls"
+	"crypto/x509"
 	"strconv"
+	"sync"
 	"testing"
+	"time"
 )
 
 func TestLen(t *testing.T) {
@@ -10,59 +14,63 @@ func TestLen(t *testing.T) {
 	t.Log(a)
 }
 
-// func TestListener(t *testing.T) {
-// 	msg1 := "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-// 	msg2 := "In sit amet lectus felis, at pellentesque turpis."
-// 	msg3 := "Nunc urna enim, cursus varius aliquet ac, imperdiet eget tellus."
-// 	msg4 := randString(45000)
-// 	msg5 := randString(500000)
-//
-// 	host := "localhost:" + Conf.App.Port
-// 	cert, privKey, _ := genCert("localhost", 0, nil, nil, 512, "localhost", "devastator")
-// 	listener, err := Listen(cert, privKey, host, Conf.App.Debug)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	defer listener.Close()
-//
-// 	go listener.Accept(func(conn *Conn, session *Session, msg []byte) {
-// 		certs := conn.ConnectionState().PeerCertificates
-// 		if len(certs) > 0 {
-// 			t.Logf("Client connected with client certificate subject: %v\n", certs[0].Subject)
-// 		}
-// 	}, func(conn *Conn, session *Session) {
-// 	})
-//
-// 	roots := x509.NewCertPool()
-// 	ok := roots.AppendCertsFromPEM(cert)
-// 	if !ok {
-// 		panic("failed to parse root certificate")
-// 	}
-//
-// 	tlsConf := &tls.Config{RootCAs: roots}
-// 	conn, err := tls.Dial("tcp", host, tlsConf)
-// 	if err != nil {
-// 		t.Fatal(err)
-// 	}
-// 	defer conn.Close()
-//
-// 	newconn := NewConn(conn, 0, 0, 0)
-//
-// 	send(t, newconn, "ping")
-// 	send(t, newconn, msg1)
-// 	send(t, newconn, msg1)
-// 	send(t, newconn, msg2)
-// 	send(t, newconn, msg3)
-// 	send(t, newconn, msg4)
-// 	send(t, newconn, msg1)
-// 	send(t, newconn, msg5)
-// 	send(t, newconn, msg1)
-// 	send(t, newconn, "close")
-//
-// 	// t.Logf("\nconn:\n%+v\n\n", conn)
-// 	// t.Logf("\nconn.ConnectionState():\n%+v\n\n", conn.ConnectionState())
-// 	// t.Logf("\ntls.Config:\n%+v\n\n", tlsConf)
-// }
+func TestListener(t *testing.T) {
+	msg1 := "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+	msg2 := "In sit amet lectus felis, at pellentesque turpis."
+	msg3 := "Nunc urna enim, cursus varius aliquet ac, imperdiet eget tellus."
+	msg4 := randString(45000)
+	msg5 := randString(500000)
+
+	host := "localhost:" + Conf.App.Port
+	cert, privKey, _ := genCert("localhost", 0, nil, nil, 512, "localhost", "devastator")
+	connwg := new(sync.WaitGroup)
+	reqwg := new(sync.WaitGroup)
+	listener, err := Listen(cert, privKey, host, connwg, reqwg, Conf.App.Debug)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer listener.Close()
+
+	go listener.Accept(func(conn *Conn, session *Session, msg []byte) {
+		certs := conn.ConnectionState().PeerCertificates
+		if len(certs) > 0 {
+			t.Logf("Client connected with client certificate subject: %v\n", certs[0].Subject)
+		}
+	}, func(conn *Conn, session *Session) {
+	})
+
+	roots := x509.NewCertPool()
+	ok := roots.AppendCertsFromPEM(cert)
+	if !ok {
+		panic("failed to parse root certificate")
+	}
+
+	tlsConf := &tls.Config{RootCAs: roots}
+	conn, err := tls.Dial("tcp", host, tlsConf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer conn.Close()
+
+	newconn := NewConn(conn, 0, 0, 0)
+
+	send(t, newconn, "ping")
+	send(t, newconn, msg1)
+	send(t, newconn, msg1)
+	send(t, newconn, msg2)
+	send(t, newconn, msg3)
+	send(t, newconn, msg4)
+	send(t, newconn, msg1)
+	send(t, newconn, msg5)
+	send(t, newconn, msg1)
+	send(t, newconn, "close")
+
+	time.Sleep(time.Millisecond * 100)
+
+	// t.Logf("\nconn:\n%+v\n\n", conn)
+	// t.Logf("\nconn.ConnectionState():\n%+v\n\n", conn.ConnectionState())
+	// t.Logf("\ntls.Config:\n%+v\n\n", tlsConf)
+}
 
 func send(t *testing.T, conn *Conn, msg string) {
 	n, err := conn.Write([]byte(msg))
