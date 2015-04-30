7a988008ef5041c77132053e178f3b652e9dc8f9
Teoman Soygul <teo@soygul.com>
2015-04-30 16:16:55 +0200

    notes on new context object

--- context.go
+++ context.go
@@ -2,14 +2,15 @@ package main
 
 // Context is a connection context.
 type Context struct {
-	UserID         uint32 // should be in state
-	IsDisconnected bool
-	err            string
-	Conn           *Conn
-	State          interface{}
+	UserID       uint32 // should be in state? as user management is not a listener package duty..
+	Disconnected bool
+	err          string
+	Conn         *Conn
+	State        interface{}
 }
 
 // SetError sets a connection error that will be written to the connection before it is closed right before the next read cycle.
 func (c *Context) SetError(err string) {
+	// todo use mutex
 	c.err = err
 }
