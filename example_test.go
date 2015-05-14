b766ac80fee1a5672639c6d8926fc140c53c4c2a
Teoman Soygul <teo@soygul.com>
2015-05-15 00:42:31 +0200

    fix example

--- example_test.go
+++ example_test.go
@@ -1,10 +1,17 @@
 package devastator_test
 
-// import "github.com/nbusy/devastator"
-//
-// // todo: add server.go setup example
-//
-// // Example demonstrating the use of Devastator server.
-// func Example() {
-// 	s := devastator.NewServer(nil, nil, "", false)
-// }
+import (
+	"log"
+
+	"github.com/nbusy/devastator"
+)
+
+// todo: add server.go setup example
+
+// Example demonstrating the use of Devastator server.
+func Example() {
+	s, err := devastator.NewServer(nil, nil, "", false)
+	if err == nil && s != nil {
+		log.Println("Connected")
+	}
+}
