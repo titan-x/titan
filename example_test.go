b1c7c0ab2d1738cb8ce16c5060f2d4739f22f3fe
Teoman Soygul <teo@soygul.com>
2015-05-15 10:46:51 +0200

    make sure that whole example file is presented in godocs

--- example_test.go
+++ example_test.go
@@ -6,9 +6,11 @@ import (
 	"github.com/nbusy/devastator"
 )
 
+const debug = false
+
 // Example demonstrating the use of Devastator server.
 func Example() {
-	s, err := devastator.NewServer(nil, nil, "", false)
+	s, err := devastator.NewServer(nil, nil, "", debug)
 	if err == nil && s != nil {
 		log.Println("Connected")
 	}
