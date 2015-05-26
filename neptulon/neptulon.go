2d3e8d0818e8d84709d6583dce603f710af03974
Teoman Soygul <tsoygul@gmail.com>
2015-05-26 02:57:48 +0200

    att top level neptulon req handlers

--- neptulon/neptulon.go
+++ neptulon/neptulon.go
@@ -2,6 +2,7 @@
 package neptulon
 
 import (
+	"log"
 	"net/http"
 	"sync"
 )
@@ -15,8 +16,49 @@ type Neptulon struct {
 	middlewares []*func(ctx Context) (response interface{})
 }
 
+// New creates and returns a new Neptulon app. This is the default TLS constructor.
+// Debug mode dumps raw TCP data to stderr (log.Println() default).
+func New(cert, privKey []byte, laddr string, debug bool) (*Neptulon, error) {
+	l, err := Listen(cert, privKey, laddr, debug)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Neptulon{
+		debug:    debug,
+		listener: l,
+		mutex:    sync.Mutex{},
+	}, nil
+}
+
 // Handle registers a new middleware to handle incoming messages.
 func (n *Neptulon) Handle() {}
+
+// Run starts accepting connections on the internal listener and handles connections with registered middleware.
+// This function blocks and never returns, unless there was an error while accepting a new connection or the listner was closed.
+func (n *Neptulon) Run() error {
+	err := n.listener.Accept(handleMsg, handleDisconn)
+	if err != nil && n.debug {
+		log.Fatalln("Listener returned an error while closing:", err)
+	}
+
+	n.mutex.Lock()
+	n.err = err
+	n.mutex.Unlock()
+
+	return err
+}
+
+// handleMsg handles incoming client messages.
+func handleMsg(conn *Conn, session *Session, msg []byte) {
+
+}
+
+// handleDisconn handles client disconnection.
+func handleDisconn(conn *Conn, session *Session) {}
+
+// UseTLS enables TLS on a Neptulon app.
+// func (n *Neptulon) UseTLS(cert, privKey []byte) {}
 
 type handler func(w http.ResponseWriter, r *http.Request) error
 
