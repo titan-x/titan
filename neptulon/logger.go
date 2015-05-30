f9aa62868f853501e0e46ddd6d0d2d2d810fbd1e
Teoman Soygul <tsoygul@gmail.com>
2015-05-31 01:15:15 +0200

    add per logger stub

--- neptulon/logger.go
+++ neptulon/logger.go
@@ -1,4 +1,9 @@
 package neptulon
 
+import "github.com/nbusy/devastator/neptulon"
+
 // Logger provides low level request logging, performance metrics, and other metrics data.
 type Logger struct{}
+
+func perfLoggerMiddleware(conn *neptulon.Conn, session *neptulon.Session, msg []byte) {
+}
