6f75058370bcd1368efbfd19cc886536761621f1
Teoman Soygul <tsoygul@gmail.com>
2015-07-04 14:23:38 +0200

    tidy up auth tests

--- test/auth_test.go
+++ test/auth_test.go
@@ -7,34 +7,6 @@ func TestAuth(t *testing.T) {
 	// t.Fatal("Anonymous calls to method.auth and method.close should be allowed")
 }
 
-func TestGoogleAuth(t *testing.T) {
-	s := NewServerHelper(t)
-	defer s.Stop()
-	c := NewClientHelper(t).Dial()
-	defer c.Close()
-
-	// t.Fatal("Google+ second sign-in (regular) failed with valid credentials")
-	// t.Fatal("Google+ sign-in passed with invalid credentials")
-	// t.Fatal("Authentication was not ACKed")
-}
-
-func TestGoogleRegister(t *testing.T) {
-	s := NewServerHelper(t)
-	defer s.Stop()
-	c := NewClientHelper(t).Dial()
-	defer c.Close()
-
-	// c.WriteRequest("auth.google", map[string]string{"accessToken": "abc"})
-	// c.ReadMsg()
-
-	// h.Client.WriteRequest("auth.google", map[string]string{"OAuthToken": "1234"})
-	// m := h.Client.ReadMsg()
-
-	// should get client cert and try connecting with it again
-
-	// t.Fatal("Google+ first sign-in/registration failed with valid credentials")
-}
-
 func TestValidClientCertAuth(t *testing.T) {
 	s := NewServerHelper(t)
 	defer s.Stop()
@@ -63,8 +35,28 @@ func TestInvalidClientCertAuth(t *testing.T) {
 	}
 }
 
-func TestTokenAuth(t *testing.T) {
-	// t.Fatal("Authentication failed with a valid token")
-	// t.Fatal("Authenticated with invalid/expired token")
+func TestGoogleRegister(t *testing.T) {
+	s := NewServerHelper(t)
+	c := NewClientHelper(t).Dial()
+
+	c.WriteRequest("auth.google", map[string]string{"accessToken": "abc"})
+	res := c.ReadRes()
+
+	// t.Fatal("Google+ first sign-in/registration failed with valid credentials")
+
+	c.Close()
+	s.Stop()
+
+	// todo: should get client cert and try connecting with it again
+}
+
+func TestGoogleAuth(t *testing.T) {
+	s := NewServerHelper(t)
+	defer s.Stop()
+	c := NewClientHelper(t).Dial()
+	defer c.Close()
+
+	// t.Fatal("Google+ second sign-in (regular) failed with valid credentials")
+	// t.Fatal("Google+ sign-in passed with invalid credentials")
 	// t.Fatal("Authentication was not ACKed")
 }
