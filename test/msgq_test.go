a8fff2d85ca3ca1a1ca7382e144b26267fdf12a8
Teoman Soygul <tsoygul@gmail.com>
2015-08-14 22:40:55 +0200

    simplify database seeding in tests

--- test/msgq_test.go
+++ test/msgq_test.go
@@ -1,18 +1,10 @@
 package test
 
-import (
-	"testing"
-
-	"github.com/nbusy/devastator"
-)
+import "testing"
 
 func TestReceiveQueue(t *testing.T) {
-	s := NewServerHelper(t)
+	s := NewServerHelper(t).SeedDB()
 	defer s.Stop()
 	c := NewClientHelper(t).DefaultCert().Dial()
 	defer c.Close()
-
-	db := s.GetDB()
-	db.SaveUser(&devastator.User{ID: 1, Cert: certChain.ClientCert})
-	db.SaveUser(&devastator.User{ID: 2})
 }
