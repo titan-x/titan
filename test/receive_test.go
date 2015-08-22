888b41b1ee855a63f16ad245c618b58481bf530c
Teoman Soygul <tsoygul@gmail.com>
2015-08-22 14:34:20 +0200

    add simple data

--- test/receive_test.go
+++ test/receive_test.go
@@ -8,7 +8,7 @@ func TestReceiveOfflineQueue(t *testing.T) {
 	c1 := NewClientHelper(t).DefaultCert().Dial()
 	defer c1.Close()
 
-	_ = c1.WriteRequest("msg.send", nil)
+	_ = c1.WriteRequest("msg.send", map[string]string{"to": "2", "msg": "How do you do?"})
 
 	c2 := NewClientHelper(t).Cert(client2Cert, client2Key).Dial()
 	defer c2.Close()
