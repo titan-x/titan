c977601afff68c96681913f1932f2fdfcdef7fa6
Teoman Soygul <tsoygul@gmail.com>
2016-02-19 13:55:19 +0100

    fix TestSendMsgOffline

--- test/msg_test.go
+++ test/msg_test.go
@@ -88,57 +88,39 @@ func TestSendMsgOnline(t *testing.T) {
 	// todo: verify that there are no pending requests for either user 1 or 2
 }
 
-//
-// func TestSendMsgOffline(t *testing.T) {
-// 	s := NewServerHelper(t).SeedDB()
-// 	defer s.Stop()
-// 	c1 := NewConnHelper(t, s).AsUser(&s.SeedData.User1).Dial()
-// 	defer c1.Close()
-//
-// 	// send message to user 2 with a basic hello message
-// 	c1.WriteRequest("msg.send", sendMsgReq{To: "2", Message: "Hello, how are you?"})
-// 	res := c1.ReadRes(nil)
-// 	if res.Result != "ACK" {
-// 		t.Fatal("Failed to send message to user 2:", res)
-// 	}
-//
-// 	// connect as user 2 and send msg.recv request to announce availability and complete client-cert auth
-// 	c2 := NewConnHelper(t, s).AsUser(&s.SeedData.User2).Dial()
-// 	defer c2.Close()
-//
-// 	c2.WriteRequest("msg.recv", nil)
-// 	res = c2.ReadRes(nil)
-// 	if res.Result != "ACK" {
-// 		t.Fatal("Failed to send msg.recv request from client 2 to server:", res)
-// 	}
-//
-// 	// receive the hello message from user 1 (online) as user 2 (was offline at the time message was sent)
-// 	var c2r recvMsgReq
-// 	c2req := c2.ReadReq(&c2r)
-// 	if c2r.From != "1" {
-// 		t.Fatal("Received message from wrong sender instead of 1:", c2r.From)
-// 	} else if c2r.Message != "Hello, how are you?" {
-// 		t.Fatal("Received wrong message content:", c2r.Message)
-// 	}
-//
-// 	c2.WriteResponse(c2req.ID, "ACK", nil)
-//
-// 	// todo: verify that there are no pending requests for either user 1 or 2
-// 	// todo: below is a placeholder since writing last ACK response will never finish as we never wait for it
-// 	c1.WriteRequest("msg.echo", map[string]string{"echo": "echo"})
-// 	resfin := c1.ReadRes(nil).Result.(map[string]interface{})["echo"]
-// 	if resfin != "echo" {
-// 		t.Fatal("Last echo did return an invalid response:", resfin)
-// 	}
-//
-// 	// todo: as client_helper is implicitly logging errors with t.Fatal(), we can't currently add useful information like below:
-// 	// t.Fatal("Failed to receive queued messages after coming online")
-// 	// t.Fatal("Failed to send ACK for received message queue")
-// }
-//
-// func TestSendAsync(t *testing.T) {
-// 	// test case to do all of the following simultaneously to test the async nature of titan server
-// 	// - cert.auth
-// 	// - msg.recv
-// 	// - msg.send (bath to multiple people where some of whom are online)
-// }
+func TestSendMsgOffline(t *testing.T) {
+	sh := NewServerHelper(t).SeedDB()
+	defer sh.ListenAndServe().CloseWait()
+
+	// get only user 1 online
+	ch1 := sh.GetClientHelper().AsUser(&sh.SeedData.User1)
+	defer ch1.Connect().JWTAuth().CloseWait()
+	ch2 := sh.GetClientHelper().AsUser(&sh.SeedData.User2)
+
+	// send a hello message from user 1
+	m := "Hello, how are you?"
+	ch1.SendMessagesSafeSync([]client.Message{client.Message{To: "2", Message: m}})
+
+	// get user 2 online receive the pending hello message
+	defer ch2.Connect().JWTAuth().CloseWait()
+	msgs := ch2.GetMessagesWait()
+	if len(msgs) != 1 {
+		t.Fatalf("expected message count: 1, got: %v", len(msgs))
+	}
+	msg := msgs[0]
+	if msg.From != "1" {
+		t.Fatalf("expected message from: 1, got: %v", msg)
+	}
+	if msg.Message != m {
+		t.Fatalf("expected message body: %v, got: %v", m, msg.Message)
+	}
+
+	// todo: verify that there are no pending requests for either user 1 or 2
+}
+
+func TestSendAsync(t *testing.T) {
+	// test case to do all of the following simultaneously to test the async nature of titan server
+	// - cert.auth
+	// - msg.recv
+	// - msg.send (bath to multiple people where some of whom are online)
+}
