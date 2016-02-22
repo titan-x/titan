7f3bb857dc715d0d2f81d4be1f1e157626f5dd58
Teoman Soygul <tsoygul@gmail.com>
2016-02-22 23:36:35 +0100

    message.to field cannot be nil

--- client/message.go
+++ client/message.go
@@ -5,7 +5,7 @@ import "time"
 // Message is a chat message.
 type Message struct {
 	From    string    `json:"from,omitempty"`
-	To      string    `json:"to,omitempty"`
+	To      string    `json:"to"`
 	Time    time.Time `json:"time"`
 	Message string    `json:"message"`
 }
