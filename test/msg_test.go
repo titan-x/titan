c004e885196df4db3d1fbb0aaafb8b2228d62d4b
Teoman Soygul <tsoygul@gmail.com>
2016-02-23 11:22:57 +0100

    relax timings for Travis once again

--- test/msg_test.go
+++ test/msg_test.go
@@ -29,8 +29,8 @@ func TestSendEcho(t *testing.T) {
 
 	select {
 	case <-gotRes:
-	case <-time.After(time.Second):
-		t.Fatal("didn't get msg.echo response in time")
+	case <-time.After(time.Second * 3):
+		t.Fatal("did not get msg.echo response in time")
 	}
 
 	// t.Fatal("Failed to send a message to the echo user")
