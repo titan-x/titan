4b2f0bea6d771f5acabdc81776da23c88939d8c2
Teoman Soygul <tsoygul@gmail.com>
2016-03-07 13:35:32 +0100

    cli: improve close error handling

--- cmd/titan/main.go
+++ cmd/titan/main.go
@@ -37,10 +37,15 @@ func startServer(addr string) {
 	if err != nil {
 		log.Fatalf("error creating server: %v", err)
 	}
-	defer s.Close()
+
+	defer func() {
+		if s.Close(); err != nil {
+			log.Printf("error closing server: %v", err)
+		}
+	}()
 
 	if err := s.ListenAndServe(); err != nil {
-		log.Fatalf("error closing server: %v", err)
+		log.Fatalf("error listening for connections: %v", err)
 	}
 }
 
