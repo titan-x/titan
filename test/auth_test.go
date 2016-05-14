7366b2e15e7421acb92916d8f81c09cbbd8a1deb
Teoman Soygul <tsoygul@gmail.com>
2016-05-14 22:10:20 +0200

    enable TestInvalidGoogleAuth

--- test/auth_test.go
+++ test/auth_test.go
@@ -78,12 +78,13 @@ func TestGoogleAuth(t *testing.T) {
 	ch.CloseWait()
 }
 
-// func TestInvalidGoogleAuth(t *testing.T) {
-// 	s := NewServerHelper(t)
-// 	defer s.Stop()
-// 	c := NewConnHelper(t, s).Dial()
-// 	defer c.Close()
-//
-// 	// t.Fatal("Google+ second sign-in (regular) failed with valid credentials")
-// 	// t.Fatal("Google+ sign-in passed with invalid credentials")
-// }
+func TestInvalidGoogleAuth(t *testing.T) {
+	sh := NewServerHelper(t).SeedDB().ListenAndServe()
+	defer sh.CloseWait()
+
+	// authenticate with Google OAuth token and get JWT token
+	// ch := sh.GetClientHelper().AsUser(&sh.SeedData.User1).Connect().GoogleAuthSync(token)
+
+	// t.Fatal("Google+ second sign-in (regular) failed with valid credentials")
+	// t.Fatal("Google+ sign-in passed with invalid credentials")
+}
