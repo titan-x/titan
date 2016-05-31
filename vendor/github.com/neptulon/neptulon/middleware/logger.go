bdce8187c75c338fd2df898a5225a742d4838f09
Teoman Soygul <tsoygul@gmail.com>
2016-05-31 15:53:17 +0200

    :arrow_up: neptulon@0.10

--- vendor/github.com/neptulon/neptulon/middleware/logger.go
+++ vendor/github.com/neptulon/neptulon/middleware/logger.go
@@ -6,6 +6,9 @@ import (
 	"github.com/neptulon/neptulon"
 )
 
+// CustResLogDataKey is the key to be used in session data store to put any custom response log data.
+const CustResLogDataKey = "CustResLogData"
+
 // Logger is an incoming/outgoing message logger.
 func Logger(ctx *neptulon.ReqCtx) error {
 	var v interface{}
@@ -13,9 +16,12 @@ func Logger(ctx *neptulon.ReqCtx) error {
 
 	err := ctx.Next()
 
-	var res = ctx.Res
-	if res == nil {
-		res = ctx.Err
+	var res interface{}
+	if res = ctx.Session.Get(CustResLogDataKey); res == nil {
+		res = ctx.Res
+		if res == nil {
+			res = ctx.Err
+		}
 	}
 
 	log.Printf("mw: logger: %v: %v, in: \"%v\", out: \"%#v\"", ctx.ID, ctx.Method, v, res)
