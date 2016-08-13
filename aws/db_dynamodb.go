83cbf0539f307103fab33e2a2c58f2187e36df89
Teoman Soygul <tsoygul@gmail.com>
2016-08-13 12:51:21 +0300

    update deps

--- aws/db_dynamodb.go
+++ aws/db_dynamodb.go
@@ -15,11 +15,21 @@ type DynamoDB struct {
 
 // NewDynamoDB creates a new AWS DynamoDB instance.
 // region = Optional region setting. Will overwrite AWS_REGION env var if available.
-func NewDynamoDB(region string) *DynamoDB {
+// endpoint = Optional endpoint URL setting. Useful for specifying local/development service URL.
+func NewDynamoDB(region string, endpoint string) *DynamoDB {
 	db := DynamoDB{}
 
-	if region != "" {
-		db.config = aws.NewConfig().WithRegion(region)
+	// carefully crafting config elements not to mess with the defaults
+	if region != "" || endpoint != "" {
+		db.config = aws.NewConfig()
+
+		if region != "" {
+			db.config.WithRegion(region)
+		}
+		if endpoint != "" {
+			db.config.WithEndpoint(endpoint)
+		}
+
 		db.session = session.New(db.config)
 	} else {
 		db.session = session.New()
