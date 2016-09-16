b85e72b8bf7f7c92b64b797d792f905f9e298422
Teoman Soygul <tsoygul@gmail.com>
2016-09-16 15:45:32 +0200

    inmemqueue: add processor func

--- data/inmem/inmemqueue.go
+++ data/inmem/inmemqueue.go
@@ -3,25 +3,23 @@ package inmem
 import (
 	"sync"
 
-	"github.com/neptulon/cmap"
 	"github.com/neptulon/neptulon"
 	"github.com/titan-x/titan/data"
 )
 
 // Queue is a message queue for queueing and sending messages to users.
 type Queue struct {
-	senderFunc SenderFunc // sender function to send and receive messages through
-	conns      *cmap.CMap // user ID -> conn ID
-
-	reqChans map[string]string // user ID -> make(chan *queuedRequest, 100)
-	mutex    sync.RWMutex      // to protect reqChans
+	senderFunc SenderFunc                // sender function to send and receive messages through
+	conns      map[string]string         // user ID -> conn ID
+	reqChans   map[string]queueProcessor // user ID -> queueProcessor
+	mutex      sync.RWMutex
 }
 
 // NewQueue creates a new queue object.
 func NewQueue(senderFunc SenderFunc) *Queue {
 	q := Queue{
-		conns:    cmap.New(),
-		reqChans: make(map[string]string),
+		conns:    make(map[string]string),
+		reqChans: make(map[string]queueProcessor),
 	}
 
 	return &q
@@ -36,46 +34,73 @@ type queuedRequest struct {
 	ResHandler func(ctx *neptulon.ResCtx) error
 }
 
+type queueProcessor struct {
+	reqChan  chan queuedRequest
+	quitChan chan bool
+}
+
+func (q *Queue) getQueueProcessor(userID string) queueProcessor {
+	c, ok := q.reqChans[userID]
+	if !ok {
+		c = queueProcessor{reqChan: make(chan queuedRequest, 5000), quitChan: make(chan bool)}
+		q.reqChans[userID] = c
+	}
+	return c
+}
+
 // Middleware registers a queue middleware to register user/connection IDs
 // for connecting users (upon their first incoming-message).
 func (q *Queue) Middleware(ctx *neptulon.ReqCtx) error {
-	uid := ctx.Conn.Session.Get("userid")
-	// set conn only once per connection
-	if _, ok := q.conns.GetOk(uid); !ok {
-		q.SetConn(uid.(string), ctx.Conn.ID)
+	q.mutex.Lock()
+	userID := ctx.Conn.Session.Get("userid").(string)
+	connID := ctx.Conn.ID
+
+	// start queue gorutine once per connection
+	if _, ok := q.conns[userID]; !ok {
+		q.conns[userID] = connID
+		data.UserCount.Add(1)
+		go q.processQueue(connID, q.getQueueProcessor(userID))
 	}
+	q.mutex.Unlock()
 
 	return ctx.Next()
 }
 
-// SetConn associates a user with a connection by ID.
-// If there are pending messages for the user, they are started to be send immediately.
-func (q *Queue) SetConn(userID, connID string) {
+// RemoveConn removes a user's associated connection ID.
+func (q *Queue) RemoveConn(userID string) {
 	q.mutex.Lock()
 	defer q.mutex.Unlock()
 
-	if _, ok := q.conns.GetOk(userID); !ok {
-		q.conns.Set(userID, connID)
-		data.UserCount.Add(1)
-		go q.processQueue()
-	}
-}
-
-// RemoveConn removes a user's associated connection ID.
-func (q *Queue) RemoveConn(userID string) {
-	q.conns.Delete(userID)
+	c := q.getQueueProcessor(userID)
+	c.quitChan <- true
+	delete(q.conns, userID)
 	data.UserCount.Add(-1)
 }
 
 // AddRequest queues a request message to be sent to the given user.
 func (q *Queue) AddRequest(userID string, method string, params interface{}, resHandler func(ctx *neptulon.ResCtx) error) error {
-	data.QueueLength.Add(1)
+	q.mutex.Lock()
+	defer q.mutex.Unlock()
 
-	// r := queuedRequest{Method: method, Params: params, ResHandler: resHandler}
+	data.QueueLength.Add(1)
+	r := queuedRequest{Method: method, Params: params, ResHandler: resHandler}
+	c := q.getQueueProcessor(userID)
+	c.reqChan <- r
 
 	return nil
 }
 
-func (q *Queue) processQueue() {
-	// tood: data.QueueLength.Add(-1) per successful request
+func (q *Queue) processQueue(connID string, processor queueProcessor) {
+	for {
+		select {
+		case req := <-processor.reqChan:
+			if _, err := q.senderFunc(connID, req.Method, req.Params, req.ResHandler); err != nil {
+				processor.reqChan <- req
+				continue
+			}
+			data.QueueLength.Add(-1)
+		case <-processor.quitChan:
+			return
+		}
+	}
 }
